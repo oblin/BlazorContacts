44fa1906b51e5520a62f60e01f9aa3fd4b4ac8d8
yiming lin <oblin228@gmail.com>
2020-01-06 17:14:26 +0800

    little clean up

--- BlazorContacts.API/Controllers/ContactsController.cs
+++ BlazorContacts.API/Controllers/ContactsController.cs
@@ -1,9 +1,6 @@
 ﻿using BlazorContacts.Shared.Models;
-
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
